badf09502be5431b886dee2957844f198bad2f8b
gay0ung <58800923+gay0ung@users.noreply.github.com>
2024-01-30 20:27:25 +0900

    chore: set eslint and prettier

--- blog-app/src/App.tsx
+++ blog-app/src/App.tsx
@@ -3,16 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello World</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
