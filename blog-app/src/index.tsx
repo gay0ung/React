f63f4b1dfca2c176c764d33880e96e3dfdf689bb
gay0ung <58800923+gay0ung@users.noreply.github.com>
2024-01-30 21:31:14 +0900

    format rules

--- blog-app/src/index.tsx
+++ blog-app/src/index.tsx
@@ -4,13 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Router>
-        <App />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
 );
